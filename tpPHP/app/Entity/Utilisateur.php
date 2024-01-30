0780192a5c6726d399435cd286799ad4172db287
Julien REBOURS <julien.rebours@etu.univ-nantes.fr>
2024-01-30 13:56:37 +0000

    Update file Utilisateur.php

--- tpPHP/app/Entity/Utilisateur.php
+++ tpPHP/app/Entity/Utilisateur.php
@@ -1,1 +1,118 @@
 <?php
+
+namespace Entity;
+
+class Utilisateur
+{
+    public $email;
+    public $pseudo;
+    public $mdp;
+    public $public;
+    public $statut;
+    public $date_inscription;
+
+    public function __construct($email, $pseudo, $mdp, $public, $statut, $date_inscription)
+    {
+        $this->email = $email;
+        $this->pseudo = $pseudi;
+        $this->mdp = $mdp;
+        $this->public = $public;
+        $this->statut = $statut;
+        $this->$date_inscription = $date_inscription;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+        /**
+     * @return mixed
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * @param mixed $pseudo
+     */
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+    }
+        /**
+     * @return mixed
+     */
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+    /**
+     * @param mixed $mdp
+     */
+    public function setMdp($mdp)
+    {
+        $this->mdp = $mdp;
+    }
+
+        /**
+     * @return mixed
+     */
+    public function getPublic()
+    {
+        return $this->public;
+    }
+
+    /**
+     * @param mixed $public
+     */
+    public function setPublic($public)
+    {
+        $this->public = $public;
+    }
+
+        /**
+     * @return mixed
+     */
+    public function getStatut()
+    {
+        return $this->statut;
+    }
+
+    /**
+     * @param mixed $statut
+     */
+    public function setStatut($statut)
+    {
+        $this->statut = $statut;
+    }
+
+        /**
+     * @return mixed
+     */
+    public function getDate_inscription()
+    {
+        return $this->date_inscription;
+    }
+
+    /**
+     * @param mixed $date_inscription
+     */
+    public function setDate_inscription($date_inscription)
+    {
+        $this->date_inscription = $date_inscription;
+    }
+}
