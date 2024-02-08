e320fff36e3e3b963819e33928257cfaa9cd198a
Baptiste <baptiste@generation-internet.biz>
2024-02-08 10:19:39 +0100

    ENV et connexion BD

--- tpPHP/vendor/composer/installed.php
+++ tpPHP/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '0a48f3a457828ea0b0db0a7976ebef38c7c65b3a',
+        'reference' => '9c64e309ab6d2f31d0ae1cadda1ff36293e2b11d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '0a48f3a457828ea0b0db0a7976ebef38c7c65b3a',
+            'reference' => '9c64e309ab6d2f31d0ae1cadda1ff36293e2b11d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
--- tpPHP/web/index.php
+++ tpPHP/web/index.php
@@ -1,2 +1,42 @@
 <?php
-require_once "../vendor/autoload.php";
+require "../vendor/autoload.php";
+
+// Charger les variables d'environnement
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
+
+// Informations de la base de données
+$dbHost = $_ENV['DB_HOST'];
+$dbPort = $_ENV['DB_PORT'];
+$dbName = $_ENV['DB_NAME'];
+$dbUser = $_ENV['DB_USER'];
+$dbPassword = $_ENV['DB_PASSWORD'];
+$dbCharset = $_ENV['DB_CHARSET'];
+
+
+
+// Chaîne de connexion PDO
+$dsn = "mysql:host=$dbHost:$dbPort;dbname=$dbName;charset=$dbCharset";
+
+try {
+    // Créer une instance de PDO
+    $pdo = new PDO($dsn, $dbUser, $dbPassword);
+
+    // Configurer PDO pour afficher les erreurs
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Utiliser la connexion PDO ici...
+
+    // Exemple : exécuter une requête
+    $stmt = $pdo->query("SELECT * FROM artiste");
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        
+    }
+
+    // Fermer la connexion
+    $pdo = null;
+} catch (PDOException $e) {
+    // Gérer les erreurs de connexion
+    echo "Erreur de connexion : " . $e->getMessage();
+    die();
+}
